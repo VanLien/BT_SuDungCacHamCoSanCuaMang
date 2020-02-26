baa15b7fc76d8a51e1df77a6c8a84cfb3f99b56d
VanLien <hoanglien89nd@gmail.com>
2020-02-26 21:57:23 +0700

    SuDungMangCoSan

---
+++ SuDungHamCoSanCuaMang/bai 3/app.js
@@ -0,0 +1,21 @@
+let str = '';
+let UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let LOWER = 'abcdefghijklmnopqrstuvwxyz';
+let result = [];
+
+for(let x=0; x<str.length; x++)
+{
+    if(UPPER.includes(str[x]))
+    {
+        result.push(str[x].toLowerCase());
+    }
+    else if(LOWER.includes(str[x]))
+    {
+        result.push(str[x].toUpperCase());
+    }
+    else
+    {
+        result.push(str[x]);
+    }
+}
+document.write(result.join(''));
